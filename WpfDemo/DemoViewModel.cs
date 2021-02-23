8921ff438b4d819b17abfb485fd38a2b48000cb3
Fernando Andreu <2480661+fernandreu@users.noreply.github.com>
2021-02-23 21:39:38 +0000

    Add project files.

---
+++ WpfDemo/DemoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using Generators;
+
+namespace WpfDemo
+{
+    public partial class DemoViewModel
+    {
+        [GenerateCommand]
+        private void ExecuteTest()
+        {
+            MessageBox.Show("Executing test method");
+        }
+    }
+}
